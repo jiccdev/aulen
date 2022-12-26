0b38f51376a529c1c4206d96de19548aba825048
Jose Ignacio <ji.ccdev@outlook.com>
2022-12-26 11:01:20 -0300

    feat: importing icons

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import React, { Fragment, useEffect, useContext } from 'react';
 import PropertiesContext from '../src/context/properties/PropertiesContext';
-
 import HeadPage from '../src/components/HeadPage/HeadPage';
 import styles from '../styles/Inicio.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { icons } from '../src/components/Icons';
 
 const Inicio = () => {
   const { properties, getProperties } = useContext(PropertiesContext);
@@ -12,12 +12,17 @@ const Inicio = () => {
     getProperties();
   }, []);
 
+  /** Icon desctructuring  */
+  const { FaUserAlt } = icons;
+
   return (
     <Fragment>
       <HeadPage title="Inicio" />
 
       <section>
-        <h1 className={styles.title}>Inicio</h1>
+        <h1 className={styles.title}>
+          Inicio <FaUserAlt />
+        </h1>
       </section>
     </Fragment>
   );
