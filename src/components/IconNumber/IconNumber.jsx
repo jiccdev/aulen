b60ddd0f27172409a2295f370ef3b7f44a429a74
David Sáez <dsaezslim@gmail.com>
2022-12-30 19:36:24 -0300

    fix: Minor changes on OurService component. New component IconNumber.

---
+++ src/components/IconNumber/IconNumber.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from '../../../styles/components/soy-propietario/IconNumber.module.css'
+
+export const IconNumber = () => {
+    return(
+        <h2 className={`${styles.number}`}>1</h2>
+    )
+}
+export default IconNumber
--- src/components/OurService/OurService.jsx
+++ src/components/OurService/OurService.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import styles from '../../../styles/components/soy-propietario/quiero-arrendar/OurService/OurService.module.css'
+import IconNumber from "../IconNumber/IconNumber"
 export const OurService = () => {
   return (
     <div className={`${styles.customCol} ${styles.OurServiceContainer}`} >
       <img className={`${styles.img}`} />
-      <h2 className={`${styles.number}`}>1</h2>
+      <IconNumber />
       <p><span className={`${styles.boldP}`}>Publica gratis</span> tu propiedad con fotos y videos profesionales.</p>
     </div>
   )
