a3f70dccfef5257ceb2680b2bae6afd536fa0196
David Sáez <dsaezslim@gmail.com>
2023-01-04 11:11:12 -0300

    fix: WhyLease section updated. IconNumber component updated. New fakeData stepsLease.

--- src/components/Section/quiero-arrendar/Steps/Steps.jsx
+++ src/components/Section/quiero-arrendar/Steps/Steps.jsx
@@ -2,15 +2,22 @@ import React from "react";
 import IconNumber from "../../../IconNumber/IconNumber";
 import styles from '../../../../../styles/components/soy-propietario/quiero-arrendar/Steps/Steps.module.css'
 
-export const Steps = () => {
-    return(
-        <div className={`${styles.customRow}`}>
-            <IconNumber />
+export const Steps = ({ steps }) => {
+  return (
+    <>
+      {steps.length > 0 ?
+        steps.map(el =>
+          <div key={el.id} className={`${styles.customRow} ${styles.stepsDiv}`}>
+            <IconNumber elNumber={el.id} />
             <div className={`${styles.customCol}`}>
-                <p className={`${styles.steps__p}`}><span className={`${styles.steps__span}`}>Establecemos una estrategia</span> de acuerdo a los objetivos y necesidades planteadas.</p>
+              <span className={`${styles.steps__span}`}>{el.span}</span>
+              <p className={`${styles.steps__p}`}> {el.p}</p>
             </div>
-        </div>
-    )
+          </div>
+        )
+        : null}
+    </>
+  )
 }
 
 export default Steps
