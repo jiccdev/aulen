a57bef89cb35a716abc2ac9193eaab59e8e04dc7
Jose Ignacio <ji.ccdev@outlook.com>
2023-01-04 12:37:09 -0300

    feat: explication components

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import React, { Fragment, useEffect, useContext } from 'react';
+import Image from 'next/image';
+import PlanFrom from '../src/components/Form/PlanFrom';
+import House from '../src/img/soy-propietario/house.jpg';
 import PropertiesContext from '../src/context/properties/PropertiesContext';
 import HeadPage from '../src/components/HeadPage/HeadPage';
 import styles from '../styles/Inicio.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 import { icons } from '../src/components/Icons';
+
+/** Bootstrap components */
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Inicio = () => {
   const { properties, getProperties } = useContext(PropertiesContext);
@@ -25,6 +30,26 @@ const Inicio = () => {
         <h1 className={styles.title}>
           Inicio <FaUserAlt />
         </h1>
+        <h1></h1>
+
+        <Row>
+          <Col>
+            <Image
+              height={500}
+              width={500}
+              src={House}
+              style={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+                borderRadius: '10px',
+              }}
+            />
+          </Col>
+          <Col>
+            <PlanFrom />
+          </Col>
+        </Row>
       </section>
     </Fragment>
   );
---
+++ src/components/Form/InicioForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/** Bootstrap components */
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+const InicioForm = () => {
+  return <div>InicioForm</div>;
+};
+
+export default InicioForm;
