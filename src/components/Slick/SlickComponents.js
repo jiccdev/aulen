00a6089593931e993ec103d6f1497fbae7b843f3
Jose Ignacio <ji.ccdev@outlook.com>
2023-01-05 17:51:52 -0300

    add new deps react-slick & react-carousel

--- src/components/Slick/SlickComponents.js
+++ src/components/Slick/SlickComponents.js
@@ -1,43 +1,9 @@
 import React from 'react';
+import Slider from 'react-slick';
 
-export const SlickArrowLeft = ({ currentSlide, slideCount, ...props }) => (
-  <div
-    {...props}
-    className={`slick-arrow-prev slick-arrow ${
-      currentSlide === 0 ? ' slick-disabled' : ''
-    }`}
-    aria-hidden="true"
-    aria-disabled={currentSlide === 0 ? true : false}
-    type="button"
-  >
-    <a
-      href="#prev"
-      className="slick-prev"
-      onClick={(ev) => ev.preventDefault()}
-    >
-      {/* <Icon name="chevron-left"></Icon> */}
-      icon
-    </a>
-  </div>
-);
 
-export const SlickArrowRight = ({ currentSlide, slideCount, ...props }) => (
-  <div
-    {...props}
-    className={`slick-arrow-next slick-arrow ${
-      currentSlide === slideCount - 3 ? ' slick-disabled' : ''
-    }`}
-    aria-hidden="true"
-    aria-disabled={currentSlide === slideCount - 3 ? ' slick-disabled' : ''}
-    type="button"
-  >
-    <a
-      href="#next"
-      className="slick-next"
-      onClick={(ev) => ev.preventDefault()}
-    >
-      {/* <Icon name="chevron-right"></Icon> */}
-      icon
-    </a>
-  </div>
-);
+const SlickComponents = () => {
+  return <div></div>;
+};
+
+export default SlickComponents;
