81bbba2de067ab9eb8d8b4e676d4026eaf91f25c
David Sáez <dsaezslim@gmail.com>
2023-01-09 17:12:43 -0300

    fix: New routes unidades-en-remate and unidades-nuevas. New fakeData investmentModel.

---
+++ pages/soy-inversionista/unidades-en-remate/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import LayoutSection from "../../../src/components/Section/LayoutSection";
+import CustomerExperience from "../../../src/components/Section/adminstracion-de-arriendo/CustomerExperience/CustomerExperience";
+
+
+export const UnidadesEnRemate = () =>{
+  return(
+    <>
+    <LayoutSection>
+      
+    </LayoutSection>
+
+    <LayoutSection>
+      <CustomerExperience />
+    </LayoutSection>
+    </>
+  )
+}
+
+
+export default UnidadesEnRemate
---
+++ src/api/fakeData/investmentModel.js
@@ -0,0 +1,18 @@
+
+export const investmentModelData = [
+  {
+    h2: 'TRANSPARENTE',
+    p: 'La propiedad siempre está a nombre de Cliente, desde la firma de compra hasta la firma de venta.',
+    img: '',
+  },
+  {
+    h2: 'RENTABLE',
+    p: 'Rentabilidad del 15% asegurada en escritura pública.',
+    img: '',
+  },
+  {
+    h2: 'SEGURO',
+    p: 'El Cliente entregará el capital asociado a la compra del inmueble una vez se haya acreditado la adjudicación a su nombre, a la instancia que corresponda.',
+    img: '',
+  },
+]
