876c7384f4d816f2917223727e7bfb7e694831a2
David Sáez <dsaezslim@gmail.com>
2023-01-10 17:10:50 -0300

    fix: New component InvestmentStep added.

--- src/components/Section/unidades-en-remate/HeroSection.jsx
+++ src/components/Section/unidades-en-remate/HeroSection.jsx
@@ -1,18 +1,11 @@
 import React from "react";
 import styles from "../../../../styles/components/soy-inversionista/HeroSection/HeroSection.module.css"
+import InvestmentStep from "./investmentStep/investmentStep";
 export const HeroSection = () => {
   return(
     <section className={`${styles.customCol}`}>
     <div className={`${styles.customCol} ${styles.mainContainer}`}>
-      <div className={`${styles.customRow} ${styles.divContainer}`}>
-        <div className={`${styles.customRow}`}>
-          <h2>1</h2>
-          <div className={`${styles.customCol}`}>
-            <h3>Hola</h3>
-            <p>Texto</p>
-          </div>
-        </div>
-      </div>
+      <InvestmentStep />
       <h2>¡Invertir es así de fácil!</h2>
     </div>
     </section>
