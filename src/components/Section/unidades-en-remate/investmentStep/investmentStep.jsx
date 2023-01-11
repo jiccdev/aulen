619afa477533e7539a846b4165d4c0b796a448fa
David Sáez <dsaezslim@gmail.com>
2023-01-10 21:23:15 -0300

    fix: investmenStep and InvestmentForm updated.

--- src/components/Section/unidades-en-remate/investmentStep/investmentStep.jsx
+++ src/components/Section/unidades-en-remate/investmentStep/investmentStep.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../../../../../styles/components/soy-inversionista/HeroSection/investmentStep/InvestmentStep.module.css"
-export const InvestmentStep = () => {
+export const InvestmentStep = ({stepData}) => {
   return(
     <div className={`${styles.customRow} ${styles.divContainer}`}>
         <div className={`${styles.customRow}`}>
