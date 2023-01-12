66f487989e664d1f17682555b5defd2abd323e54
Jose Ignacio <ji.ccdev@outlook.com>
2023-01-12 15:03:22 -0300

    feat: add navigation footer

--- src/components/FooterPage/components/SocialMediaFooter.jsx
+++ src/components/FooterPage/components/SocialMediaFooter.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import styles from '../../../../styles/components/FooterPage.module.css';
 
 const SocialMediaFooter = () => {
-  return <div>SocialMediaFooter</div>;
+  return (
+    <ul>
+      <li>
+        <a
+          href="https://www.facebook.com/aulenpropiedades"
+          className={styles.navLinkFooter}
+        >
+          Face
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.instagram.com/aulenpropiedades/"
+          className={styles.navLinkFooter}
+        >
+          Insta
+        </a>
+      </li>
+      <li>
+        <a href="https://www.youtube.com" className={styles.navLinkFooter}>
+          Linkedin
+        </a>
+      </li>
+    </ul>
+  );
 };
 
 export default SocialMediaFooter;
