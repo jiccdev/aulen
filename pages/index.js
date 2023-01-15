9b3da11bbf6234df02ff2368651083309898caf8
Jose Ignacio <ji.ccdev@outlook.com>
2023-01-15 18:55:13 -0300

    feat: add lease list data

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,20 @@
 import React, { Fragment, useState, useEffect, useContext } from 'react';
-import Image from 'next/image';
 import Inicio from './inicio';
+import Lease from '../src/components/Section/inicio/Lease';
 
-import PlanFrom from '../src/components/Form/PlanFrom';
-import House from '../src/img/soy-propietario/house.jpg';
 import HeadPage from '../src/components/HeadPage/HeadPage';
-import styles from '../styles/Inicio.module.css';
-import { icons } from '../src/components/Icons';
-
-/** Bootstrap components */
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
-  /** Icon desctructuring  */
-  const { FaUserAlt } = icons;
-
   return (
     <Fragment>
       <HeadPage title="Inicio" />
 
       {/* INICIO */}
       <Inicio />
+
+      {/* NOS ENCARGAMOS DE TÚ ARRIENDO POR TÍ */}
+      <Lease />
     </Fragment>
   );
 };
---
+++ src/api/fakeData/lease.js
@@ -0,0 +1,40 @@
+import SoldHome from '../../assets/img/SoldHome/sold-home.jpg';
+
+export const leaseData = [
+  {
+    id: 1,
+    title: 'Corretaje tradicional',
+    src: SoldHome,
+    itemsList: [
+      'Estudio y Evaluación comercial. ',
+      'Fotos y videos profesionales.',
+      'Ofertas formales garantizadas. ',
+      'Cerramos tu arriendo sin necesidad de salir de tu.',
+      'casa.Acta de entrega con respaldo fotográfico.',
+    ],
+  },
+  {
+    id: 2,
+    title: 'Administración inteligente',
+    src: SoldHome,
+    itemsList: [
+      'Estudio y Evaluación comercial. ',
+      'Fotos y videos profesionales.',
+      'Ofertas formales garantizadas. ',
+      'Cerramos tu arriendo sin necesidad de salir de tu.',
+      'casa.Acta de entrega con respaldo fotográfico.',
+    ],
+  },
+  {
+    id: 3,
+    title: 'Administración con garantía',
+    src: SoldHome,
+    itemsList: [
+      'Estudio y Evaluación comercial. ',
+      'Fotos y videos profesionales.',
+      'Ofertas formales garantizadas. ',
+      'Cerramos tu arriendo sin necesidad de salir de tu.',
+      'casa.Acta de entrega con respaldo fotográfico.',
+    ],
+  },
+];
