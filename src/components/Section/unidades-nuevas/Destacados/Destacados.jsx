5df2e9fe5f2986971aec19b1fd00d6ad08adff2f
David Sáez <dsaezslim@gmail.com>
2023-01-20 12:30:40 -0300

    fix(component):Quick fix at bootstrap import.

--- src/components/Section/unidades-nuevas/Destacados/Destacados.jsx
+++ src/components/Section/unidades-nuevas/Destacados/Destacados.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from "../../../../../styles/components/soy-inversionista/unidades-nuevas/Destacados.module.css"
 import FeaturedCard from "../FeaturedCard"
 import { icons } from "../../../Icons"
