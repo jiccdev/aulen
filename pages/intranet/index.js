d2e115ab5822860a0d6e7dacd50a4d8407e605f4
Jose Ignacio <ji.ccdev@outlook.com>
2023-01-20 15:09:28 -0300

    feat: ended intranet module

--- pages/intranet/index.js
+++ pages/intranet/index.js
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
+import HeadPage from '../../src/components/HeadPage/HeadPage';
 import Login from '../../src/components/Section/intranet/Login';
 
 const Intranet = () => {
   return (
     <Fragment>
+      <HeadPage title="Intranet" />
+
       {/* LOGIN FORM - INTRANET */}
       <Login />
     </Fragment>
