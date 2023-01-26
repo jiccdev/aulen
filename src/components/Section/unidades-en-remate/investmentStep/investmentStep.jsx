aa5daab8a362dc72dc8368d3dc843f5120761879
David Sáez <dsaezslim@gmail.com>
2023-01-25 23:14:37 -0300

    fix(style): Mobile design updated.

--- src/components/Section/unidades-en-remate/investmentStep/investmentStep.jsx
+++ src/components/Section/unidades-en-remate/investmentStep/investmentStep.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "../../../../../styles/components/soy-inversionista/HeroSection/investmentStep/InvestmentStep.module.css"
 export const InvestmentStep = ({ stepData }) => {
   return (
-    <div key={stepData.id} className={`${styles.customRow} ${styles.divContainer} ${stepData.style}`}>
+    <div key={stepData.id} className={`${styles.customRow}  ${stepData.style} ${styles.divContainer}`}>
       {stepData.img}
-      <div className={`${styles.customRow}`}>
+      <div className={`${styles.customRow} ${styles.idtext}`}>
         <h2 className={`${styles.step__h2}`}>{stepData.id}</h2>
         <div className={`${styles.customCol} ${styles.textContainer}`}>
           <h3 className={`${styles.step__h3}`}>{stepData.h3}</h3>
