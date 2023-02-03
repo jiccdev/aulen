c47d828525c274d552fcbfa4a52c60861eedf9cb
Jose Ignacio <ji.ccdev@outlook.com>
2023-02-02 23:59:26 -0300

    feat(page): add lease styles to section

--- src/components/Section/HeaderSection.jsx
+++ src/components/Section/HeaderSection.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styles from '../../../styles/components/HeaderSection.module.css';
 
-const HeaderSection = ({ titleSection }) => {
+const HeaderSection = ({ titleSection, fontSize }) => {
   return (
     <div className={styles.containerSection}>
-      <h2 className={styles.h2}>{titleSection || ''}</h2>
+      <h2 className={fontSize ? styles.h2_3xl : styles.h2}>
+        {titleSection || ''}
+      </h2>
     </div>
   );
 };
--- src/components/Section/inicio/Prinicipal.jsx
+++ src/components/Section/inicio/Prinicipal.jsx
@@ -13,9 +13,12 @@ import styles from '../../../../styles/components/Inicio/Principal.module.css';
 const Prinicipal = () => {
   return (
     <div className={styles.principalContainer}>
-      <HeaderSection titleSection="¡Aulén, contigo en cada paso!" />
+      <HeaderSection
+        titleSection="¡Aulén, contigo en cada paso!"
+        fontSize="3xl"
+      />
       <Row className={styles.rowPlanForm}>
-        <Col sm={12} lg={7} className={styles.principalCol}>
+        <Col sm={12} lg={6} className={styles.principalCol}>
           <FadeComponent left cascade opposite duration={1000}>
             <Image
               width="100%"
@@ -25,7 +28,7 @@ const Prinicipal = () => {
             />
           </FadeComponent>
         </Col>
-        <Col sm={12} lg={5} className={styles.principalCol}>
+        <Col sm={12} lg={6} className={styles.principalCol}>
           <PropertyForm
             titleContentForm="Vende o arrienda tu propiedad rápido y sin complicaciones"
             textAlign="center"
